2f7cc514939340a85a4104030f57cabbcda85e7d
LegeasArthur <98401409+LegeasArthur@users.noreply.github.com>
2022-06-16 20:29:15 +0200

    Ajout d'une directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{pokemonList[1]}}!</h1>`
+  templateUrl: `app.component.html`
 
 })
 export class AppComponent implements OnInit{
-  pokemonList =  ['Bulbizarre', 'Salamèche', 'Carapuce'];
+  pokemonList:Pokemon[] = POKEMONS ;
+  pokemonSelected: Pokemon|undefined;
 
   ngOnInit(){
     console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: Pokemon){
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if (pokemon) {
+      console.log(`vous avez demandé le pokémon 
+    ${pokemon.name}`);
+    this.pokemonSelected = pokemon;
+    }else{
+      console.log(`vous avez demandez un pokemon qui n'existe pas`);
+      this.pokemonSelected = pokemon;
+    }
+    
   }
 }
